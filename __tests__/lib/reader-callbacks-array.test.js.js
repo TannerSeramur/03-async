1474a3417686175efc13f7ef517b7ce876f95375
Tanner Seramur <seramurt@gmail.com>
2018-12-20 00:50:25 -0800

    part 2 task 2

--- __tests__/lib/reader-callbacks-array.test.js.js
+++ __tests__/lib/reader-callbacks-array.test.js.js
@@ -2,12 +2,17 @@
 
 jest.mock('fs');
 
-const reader = require('../../lib/reader-callbacks-array.js');
+const reader = require('../../lib/reader-fixed.js');
 
 describe('File Reader Module', () => {
 
   it('when given a bad file, returns an error', () => {
     let files = ['bad.txt'];
+    // In jest, throwing errors obviously kills the app, so if you're
+    // going to throw one in a test, have the expect execute your code as a
+    // function so that you can trap it.
+    // reader(files, (err,data) => {
+      // console.log(err, 'ERROR HERE');
       expect(()=> {
         reader(files,(err.data) = {});
       }).toThrow();
@@ -19,7 +24,6 @@ describe('File Reader Module', () => {
   it('reads 3 files', done => {
     let files = ['file1.txt', 'file2.txt', 'file3.txt'];
     reader(files, (err,data) => {
-      console.log(err, '3RRRRRRRR')
       console.log(data, 'DATA HERE');
       expect(err).toBeUndefined();
       expect(data instanceof Array ).toBeTruthy();
